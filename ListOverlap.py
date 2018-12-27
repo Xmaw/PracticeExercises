535c9f32514d40a13bf6d6d2b3d983730be8bb94
Xmaw <ej222up@student.lnu.se>
2018-12-27 21:02:00 +0100

    LostOverlap optimized in number of lines needed

--- ListOverlap.py
+++ ListOverlap.py
@@ -5,20 +5,8 @@ Make sure your program works on two lists of different sizes.
 """
 import random
 
-a = []
-b = []
-
-a_size = random.randrange(50)
-for num in range(1, a_size):
-    a.append(random.randrange(100))
-
-b_size = random.randrange(50)
-for num in range(1, b_size):
-    b.append(random.randrange(100))
-
-l_overlap = []
-for number in a:
-    if b.__contains__(number) and not l_overlap.__contains__(number):
-        l_overlap.append(number)
+a = random.sample(range(1, 100), random.randrange(50))
+b = random.sample(range(1, 100), random.randrange(50))
+l_overlap = [over for over in a if b.__contains__(over)]
 
 print(l_overlap)
